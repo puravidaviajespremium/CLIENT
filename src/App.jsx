b1fa4f65ac972d6617c059ea1e200c07138b2120
camicastro <camicastro.11.94@gmail.com>
2023-10-10 14:16:13 -0300

    primer commit del front

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Pura Vida Viajes Premium</h1>
+      <h3 className="read-the-docs"> Aqui comienza la aventura! </h3>
+    </>
+  )
+}
+
+export default App
