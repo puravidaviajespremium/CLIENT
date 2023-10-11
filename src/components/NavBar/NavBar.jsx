15bb8f9967dc05ab6ebe3a339ad9b40351dd1d89
SDAguilar2017 <sergiodarioaguilar2017@gmail.com>
2023-10-11 00:43:27 -0300

    Rama de Prueba

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,14 +1,13 @@
-
-
-function NavBar(){
-    return(
-        <div>
-            <h1>PURA VIDA VIAJES</h1>
-            <h2>Home</h2>
-            <h2>Destinos</h2>
-            <h2>Contacto</h2>
-        </div>
-    )
+function NavBar() {
+  //Hola Keny
+  return (
+    <div>
+      <h1>PURA VIDA VIAJES</h1>
+      <h2>Home</h2>
+      <h2>Destinos</h2>
+      <h2>Contacto</h2>
+    </div>
+  );
 }
 
-export default NavBar
+export default NavBar;
