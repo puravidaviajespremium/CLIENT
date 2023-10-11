93b61a6a7283cc5dcedc8f981066ade5219e591c
julian.castaño_a <juliancaristizabal@gmail.com>
2023-10-10 20:25:27 -0500

    rutas y carpetas

---
+++ src/views/Home/Home.jsx
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom";
+import NavBar from ""
+
+function Home() {
+    return (
+        <div>
+            <div>
+                <NavBar />
+                <h1>Comience su viaje con nosotros.</h1>
+                <p>Destinos de Ensueño. Experiencias inolvidables...</p>
+            </div>
+        </div>
+    )
+}
+
+export default Home;
