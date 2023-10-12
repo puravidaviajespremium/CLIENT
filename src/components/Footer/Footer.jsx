3cda3b457854e2600e3ff5426f2bba77e4643156
camicastro <camicastro.11.94@gmail.com>
2023-10-12 03:33:58 -0300

    footer listo

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import styles from './Footer.module.css';
 import { Link } from 'react-router-dom';
-import { BsTwitter, BsFacebook, BsInstagram, BsPinterest } from 'react-icons/bs'
+import { BsFacebook, BsInstagram } from 'react-icons/bs'
 
 const Footer = () => {
 //
     return (
 
-        <div className={styles.divFooter}>
-                <div className={styles.divfooter}>
+        <footer className={styles.divFooter}>
+                <div className={`${styles.divfooter} container`}>
 
-                    <img className={styles.imgLogo} src='https://cdn.discordapp.com/attachments/1159312188352245847/1161863081081700372/logo-puraVidaPremium-blanco.png?ex=6539d850&is=65276350&hm=654dff3cdae8e88379d59655c8dda826d981bfefa286839f4763e9c701e03923&' alt="Logo" />
+                    <img className={styles.imgLogo} src='/logo-empresa_2.png' alt="" />
                     <div className={styles.divSecciones}>
                         <h3>Contacto</h3>
                         <p>+506 6218 2218</p>
@@ -33,7 +33,7 @@ const Footer = () => {
                     </div>
 
                     <div className={styles.divSecciones}>
-                        <h3>Seguinos en</h3>
+                        <h3>Síguenos en</h3>
                         <div className={styles.divRedes}>
                             <a href='https://www.facebook.com/PuraVidaPremiumTravelExperience/'> <p><BsFacebook/></p> </a>
                             <a href='https://instagram.com/puravidapremiumtravelxperience?igshid=MWZjMTM2ODFkZg=='> <p> <BsInstagram/> </p></a>
@@ -46,7 +46,7 @@ const Footer = () => {
             
             <p>Copyright © Pura Vida Viajes 2023 All rights reserved</p>
 
-        </div>
+        </footer>
         
             
     )
