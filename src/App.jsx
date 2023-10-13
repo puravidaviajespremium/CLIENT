2232412196d18490f722cdacfe4e911bfa3aeb2a
Melodi Balossino <melodibalossino@gmail.com>
2023-10-13 01:55:09 -0300

    Componente Faqs

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from 'react-router-dom'
 import Home from './views/Home/Home'
+import Faqs from './views/FAQs/Faqs'
 import './App.css'
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <>
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/faqs' element={<Faqs />} />
       </Routes>
     </>
   )
