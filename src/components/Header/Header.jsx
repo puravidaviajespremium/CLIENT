b593fbd82b3b76228930978fe289fd0c80237fc6
julian.castaño_a <juliancaristizabal@gmail.com>
2023-10-13 16:45:04 -0500

    NavBar correciones

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,88 @@
-import NavBar from "../../components/NavBar/NavBar"
+import { useState } from "react"
+import { Link } from "react-router-dom"
+import { AiOutlineMenu, AiOutlineClose, AiOutlineUser } from 'react-icons/ai'
+import { LuSearch } from 'react-icons/lu'
 import styles from "./Header.module.css"
 
+
 function Header() {
+    const [toggleMobMenu, setToggleMobMenu] = useState(false);
+    const [toggleUserMenu, setToggleUserMenu] = useState(false);
+    const [toggleSearch, setToggleSearch] = useState(false);
+
+    const toggleMobileMenu = () => {
+        setToggleMobMenu(!toggleMobMenu)
+    }
+
+    const toggleUMenu = () => {
+        setToggleUserMenu(!toggleUserMenu)
+    }
+
+    const toggleInput = () => {
+        setToggleSearch(!toggleSearch)
+    }
+
     return (
-        <div className={styles.headerCont}>
-            <NavBar />
-            <div className={styles.slogan}>
-                <h1>Comience su <br/>viaje con nosotros.</h1>
-                <p>Destinos de Ensueño. Experiencias inolvidables...</p>
+        <header className={styles.headerCont}>
+            <div className={`${styles.headerCont} container`}>
+
+
+                <img className={styles.imgLogo} src='/images/logo-empresa_2.png' alt="" />
+
+                <nav className={styles.deskMenu}>
+                    <Link to="/">Home</Link>
+                    <Link to="/destinos">Destinos</Link>
+                    <Link to="/contacto">Contacto</Link>
+                </nav>
+
+
+
+                <div className={styles.menus} >
+                    <div >
+                        <span className={styles.searchIcon} onClick={toggleInput}>
+                            <LuSearch />
+                        </span>
+                    {/* <div>
+                        <input type="text" placeholder="Buscar pais"/>
+                    </div> */}
+
+                    </div>
+                    {/* DESKTOP */}
+                    <span className={styles.userIcon} onClick={toggleUMenu}>
+                        <AiOutlineUser />
+                    </span>
+                    <div className={styles.desktopHeader}>
+                        <div className={styles.contUM}>
+                            <nav className={`${styles.userMenu}  ${toggleUserMenu && styles.userMenuActive}`}>
+                                <ul>
+                                    <Link to="/register"><li>Sign in</li></Link>
+                                </ul>
+                            </nav>
+                        </div>
+                    </div>
+
+
+                    {/* MOBILE */}
+                    <div className={styles.mobileHeader}>
+                        <span className={styles.burgerIcon} onClick={toggleMobileMenu}>
+                            {toggleMobMenu ? <AiOutlineClose /> : <AiOutlineMenu />}
+                        </span>
+
+                        <nav className={`${styles.mobileMenuCont}  ${toggleMobMenu && styles.mobileMenuContActive}`}>
+                            <ul>
+                                <Link to="/"><li>Home</li></Link>
+                                <Link to="/destinos"><li>Destinos</li></Link>
+                                <Link to="/contacto"><li>Contacto</li></Link>
+                                <Link to="/SingIn"><li>Sign in</li></Link>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+
+
             </div>
-        </div>
+        </header>
     )
 }
 
-export default Header;
+export default Header
--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,19 +1,16 @@
-
 import Header from "../../components/Header/Header";
-import styles from "./Home.module.css"
-import NavBar from "../../components/NavBar/NavBar"
 import Footer from "../../components/Footer/Footer";
+import styles from "./Home.module.css"
 
 function Home() {
     return (
         <>
             <Header />
-            
+
             <main>Body</main>
-            
-            
-            <Footer/>
-            
+
+            <Footer />
+
         </>
     )
 }
