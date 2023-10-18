0413899ea1c77086c1f1be02315476590e4d6425
julian.castaño_a <juliancaristizabal@gmail.com>
2023-10-18 01:51:48 -0500

    Componentes de promocion/ComponenteError404

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -33,11 +33,6 @@ function Home() {
                 
             </section>
 
-            {/* Los puse aqui para ver */}
-            <PromocionII/>
-            <PromocionIII/>
-
-
             <Information/>
             <Promocion/>
         </>
