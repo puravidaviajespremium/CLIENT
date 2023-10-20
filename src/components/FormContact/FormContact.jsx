d30fd703b88902a46360a508d05d1732b47ec2e4
Keny Espinoza Luis <kespinozaluis@gmail.com>
2023-10-20 17:36:59 -0500

    Estandarizando estilos del formulario de contacto

--- src/components/FormContact/FormContact.jsx
+++ src/components/FormContact/FormContact.jsx
@@ -1,5 +1,11 @@
 import { useState} from "react";
-import {BsFillExclamationCircleFill, BsFillInfoCircleFill} from "react-icons/bs";
+import {
+  BsFillExclamationCircleFill, 
+  BsFillInfoCircleFill,
+  BsFacebook,
+  BsInstagram,
+  BsFillEnvelopeFill,
+  BsHeadset} from "react-icons/bs";
 import validate from "./validate.js";
 import { addClient } from "../../redux/actions/clientsActions";
 import { useDispatch, useSelector } from "react-redux";
@@ -76,6 +82,36 @@ const FormContact = () => {
   };
   return (
     <div className={styles.contactContainer}>
+      <div className={styles.containerInfo}>
+          <div className={styles.contactInfo}>
+            <BsFillEnvelopeFill className={styles.icon} />
+            <div className={styles.info}>
+              <h4>Correo Electrónico</h4>
+              <p>info@viajespuravidapremium.com</p>
+            </div>
+          </div>
+          <div className={styles.contactInfo}>
+            <BsHeadset className={styles.icon} />
+            <div className={styles.info}>
+              <h4>Teléfono</h4>
+              <p>+506 6218 2218</p>
+            </div>
+          </div>
+          <div className={styles.contactInfo}>
+            <div className={styles.social}>
+              <a href="https://www.facebook.com/PuraVidaPremiumTravelExperience/">
+                <BsFacebook className={styles.icon} />
+              </a>
+              <a href="https://instagram.com/puravidapremiumtravelxperience?igshid=MWZjMTM2ODFkZg==">
+                <BsInstagram className={styles.icon} />
+              </a>
+            </div>
+            <div className={styles.info}>
+              <h4>Redes Sociales</h4>
+              <p>Visítanos</p>
+            </div>
+          </div>
+        </div>
       <div className={styles.containerform}>
         <h1 className={styles.titleForm}>Contáctanos</h1>
         <p>
--- src/views/Contact/Contact.jsx
+++ src/views/Contact/Contact.jsx
@@ -1,9 +1,3 @@
-import {
-  BsFacebook,
-  BsInstagram,
-  BsFillEnvelopeFill,
-  BsHeadset
-} from "react-icons/bs";
 import FormContact from "../../components/FormContact/FormContact";
 import styles from "./Contact.module.css"
 
@@ -13,36 +7,6 @@ const Contact = () => {
     <section>
       <div className={styles.contactBanner}>
         <h1 className={styles.titleBanner}>Contacto</h1>
-        <div className={styles.containerInfo}>
-          <div className={styles.contactInfo}>
-            <BsFillEnvelopeFill className={styles.icon} />
-            <div className={styles.info}>
-              <h4>Correo Electrónico</h4>
-              <p>info@viajespuravidapremium.com</p>
-            </div>
-          </div>
-          <div className={styles.contactInfo}>
-            <BsHeadset className={styles.icon} />
-            <div className={styles.info}>
-              <h4>Teléfono</h4>
-              <p>+506 6218 2218</p>
-            </div>
-          </div>
-          <div className={styles.contactInfo}>
-            <div className={styles.social}>
-              <a href="https://www.facebook.com/PuraVidaPremiumTravelExperience/">
-                <BsFacebook className={styles.icon} />
-              </a>
-              <a href="https://instagram.com/puravidapremiumtravelxperience?igshid=MWZjMTM2ODFkZg==">
-                <BsInstagram className={styles.icon} />
-              </a>
-            </div>
-            <div className={styles.info}>
-              <h4>Redes Sociales</h4>
-              <p>Visítanos</p>
-            </div>
-          </div>
-        </div>
       </div>
       <FormContact/>
     </section>
