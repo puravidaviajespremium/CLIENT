3d66dfaea21a4854e536729c211ca8b57cf3c30c
camicastro <camicastro.11.94@gmail.com>
2023-10-25 14:18:01 -0300

    redireccion usuario a paypal LISTO

--- src/views/AdminPanel/dataProvider.js
+++ src/views/AdminPanel/dataProvider.js
@@ -1,25 +1,14 @@
 import axios from 'axios';
 const apiUrl = "http://localhost:3001/"
 
-const customDataProvider = {
+const dataProvider = {
   getList: (resource, params) => {
-    const { page, perPage } = params.pagination;
-    const start = (page - 1) * perPage;
-    const end = page * perPage - 1;
-
-    return axios.get(`${apiUrl}${resource}`, {
-      params: {
-        _start: start,
-        _end: end,
-        _sort: params.sort.field,
-        _order: params.sort.order,
-      }
-    })
-    .then(response => ({
-      data: response.data,
-      total: parseInt(response.headers['x-total-count'], 10 || 0),
-    }));
+    return axios.get(`${apiUrl}${resource}`)
+      .then(response => ({
+        data: response.data,
+        total: response.data.length, // Esto puede variar según tu backend
+      }));
   },
 };
 
-export default customDataProvider;
+export default dataProvider;
